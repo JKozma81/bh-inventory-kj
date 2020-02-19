6893e55486437b313b5aec365d1d90c411564e76
Kozma János <41972874+JKozma81@users.noreply.github.com>
2020-02-19 18:41:15 +0100

    Stylling

--- inventorysrv.js
+++ inventorysrv.js
@@ -7,10 +7,43 @@ const app = express();
 app.engine('handlebars', hbs());
 app.set('view engine', 'handlebars');
 
+const dummyData = [
+    {
+        id: Math.floor(Math.random() * 100 + 1),
+        name: 'Processzor',
+        category: 'Számítástechnika'
+    },
+    {
+        id: Math.floor(Math.random() * 100 + 1),
+        name: 'Winchester',
+        category: 'Számítástechnika'
+    },
+    // {
+    //     id: Math.floor(Math.random() * 100 + 1),
+    //     name: 'Mosogatógép',
+    //     category: 'Konyhatechnika'
+    // },
+    // {
+    //     id: Math.floor(Math.random() * 100 + 1),
+    //     name: 'Elektromos radiátor',
+    //     category: 'Fűtéstechnika'
+    // },
+    // {
+    //     id: Math.floor(Math.random() * 100 + 1),
+    //     name: 'Billentyűzet',
+    //     category: 'Számítástechnika'
+    // },
+    // {
+    //     id: Math.floor(Math.random() * 100 + 1),
+    //     name: 'Vezeték nélküli egér',
+    //     category: 'Számítástechnika'
+    // }
+]
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { title: 'Termékek', items: dummyData });
 })
 
 
