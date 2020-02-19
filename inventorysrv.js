68eb1183a245091e38bcb4b37c6b5ac9e9589f8b
JKozma81 <janos.kozma81@gmail.com>
2020-02-19 23:22:41 +0100

    First page styling finished

--- inventorysrv.js
+++ inventorysrv.js
@@ -8,43 +8,42 @@ app.engine('handlebars', hbs());
 app.set('view engine', 'handlebars');
 
 const dummyData = [
-    {
-        id: Math.floor(Math.random() * 100 + 1),
-        name: 'Processzor',
-        category: 'Számítástechnika'
-    },
-    {
-        id: Math.floor(Math.random() * 100 + 1),
-        name: 'Winchester',
-        category: 'Számítástechnika'
-    },
-    // {
-    //     id: Math.floor(Math.random() * 100 + 1),
-    //     name: 'Mosogatógép',
-    //     category: 'Konyhatechnika'
-    // },
-    // {
-    //     id: Math.floor(Math.random() * 100 + 1),
-    //     name: 'Elektromos radiátor',
-    //     category: 'Fűtéstechnika'
-    // },
-    // {
-    //     id: Math.floor(Math.random() * 100 + 1),
-    //     name: 'Billentyűzet',
-    //     category: 'Számítástechnika'
-    // },
-    // {
-    //     id: Math.floor(Math.random() * 100 + 1),
-    //     name: 'Vezeték nélküli egér',
-    //     category: 'Számítástechnika'
-    // }
-]
+	{
+		id: Math.floor(Math.random() * 100 + 1),
+		name: 'Processzor',
+		category: 'Számítástechnika'
+	},
+	{
+		id: Math.floor(Math.random() * 100 + 1),
+		name: 'Winchester',
+		category: 'Számítástechnika'
+	},
+	{
+		id: Math.floor(Math.random() * 100 + 1),
+		name: 'Mosogatógép',
+		category: 'Konyhatechnika'
+	},
+	{
+		id: Math.floor(Math.random() * 100 + 1),
+		name: 'Elektromos radiátor',
+		category: 'Fűtéstechnika'
+	},
+	{
+		id: Math.floor(Math.random() * 100 + 1),
+		name: 'Billentyűzet',
+		category: 'Számítástechnika'
+	},
+	{
+		id: Math.floor(Math.random() * 100 + 1),
+		name: 'Vezeték nélküli egér',
+		category: 'Számítástechnika'
+	}
+];
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('home', { title: 'Termékek', items: dummyData });
-})
+	res.render('home', { title: 'Termékek', items: dummyData });
+});
 
-
-app.listen(PORT, () => console.log(`App is started and listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`App is started and listening on port ${PORT}`));
