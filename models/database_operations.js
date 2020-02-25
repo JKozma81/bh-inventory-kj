0e91551f048edb7b57e965c6eb0b97ab79f8b216
Kozma János <41972874+JKozma81@users.noreply.github.com>
2020-02-25 14:54:44 +0100

    working on new features

--- models/database_operations.js
+++ models/database_operations.js
@@ -4,34 +4,40 @@ const db = new sqlite3.Database('inventory.db');
 
 function db_getAll(sqlQuery) {
 	return new Promise((resolve, reject) => {
+		// const db = new sqlite3.Database('inventory.db');
 		db.serialize(() => {
 			db.all(sqlQuery, (err, results) => {
 				if (err !== null) reject(err);
 				resolve(results);
 			});
 		});
+		// db.close();
 	});
 }
 
 function db_get(sqlQuery) {
 	return new Promise((resolve, reject) => {
+		// const db = new sqlite3.Database('inventory.db');
 		db.serialize(() => {
 			db.get(sqlQuery, (err, result) => {
 				if (err !== null) reject(err);
 				resolve(result);
 			});
 		});
+		// db.close();
 	});
 }
 
 function db_run(sqlQuery) {
 	return new Promise((resolve, reject) => {
+		// const db = new sqlite3.Database('inventory.db');
 		db.serialize(() => {
 			db.run(sqlQuery, (err) => {
 				if (err !== null) reject(err);
 				resolve();
 			});
 		});
+		// db.close();
 	});
 }
 
--- models/products_middlewares.js
+++ models/products_middlewares.js
@@ -3,10 +3,12 @@ const { db_get, db_getAll, db_run } = require('./database_operations');
 const getAllProducts = async (req, res, next) => {
     try {
 
+        const allProductSQL = 'SELECT products.id, products.name, products.description, GROUP_CONCAT(categories.category_name, ", ") as category FROM products LEFT JOIN product_groups ON products.id = product_groups.product_id LEFT JOIN categories ON product_groups.category_id = categories.id GROUP BY products.id, products.name, products.description'
+
         if (req.query.filter_category) {
 
             const filteredProducts = await db_getAll(
-                `SELECT products.id, products.name, products.description, categories.category_name as category FROM products LEFT JOIN product_groups ON products.id = product_groups.product_id LEFT JOIN categories ON product_groups.category_id = categories.id WHERE categories.category_name = "${req.query.filter_category}"`
+                `SELECT products.id, products.name, products.description, GROUP_CONCAT(categories.category_name, ", ") as category FROM products LEFT JOIN product_groups ON products.id = product_groups.product_id LEFT JOIN categories ON product_groups.category_id = categories.id WHERE categories.category_name = "${req.query.filter_category}" GROUP BY products.id, products.name, products.description`
             );
 
             const categories = await db_getAll('SELECT * FROM categories');
@@ -17,7 +19,7 @@ const getAllProducts = async (req, res, next) => {
         } else {
 
             const productsWithCategs = await db_getAll(
-                'SELECT products.id, products.name, products.description, categories.category_name as category FROM products LEFT JOIN product_groups ON products.id = product_groups.product_id LEFT JOIN categories ON product_groups.category_id = categories.id'
+                'SELECT products.id, products.name, products.description, GROUP_CONCAT(categories.category_name, ", ") as category FROM products LEFT JOIN product_groups ON products.id = product_groups.product_id LEFT JOIN categories ON product_groups.category_id = categories.id GROUP BY products.id, products.name, products.description'
             );
 
             const categories = await db_getAll('SELECT * FROM categories');
@@ -71,28 +73,44 @@ const modifyProduct = async (req, res, next) => {
         const itemId = req.params.id;
         const { product_name, product_cat, product_desc } = req.body;
 
+
+        console.log('req.body', req.body)
+
         if (product_name && product_cat) {
-            const categoryID = await db_get(`SELECT id FROM categories WHERE category_name ="${product_cat}"`);
             await db_run(
                 `UPDATE products SET name = "${product_name}", description = "${product_desc}" WHERE id = ${+itemId}`
             );
 
-            const categs = await db_get(`SELECT * FROM product_groups WHERE product_id = ${+itemId}`);
+            await db_run(`DELETE FROM product_groups WHERE product_id = ${+itemId}`);
 
-            if (categs) {
-                await db_run(
-                    `UPDATE product_groups SET category_id = "${categoryID.id}" WHERE product_id = ${+itemId}`
-                );
-                next();
-            } else {
-                await db_run(
-                    `INSERT INTO product_groups (category_id, product_id) VALUES (${categoryID.id}, ${+itemId})`
-                );
+            if (typeof product_cat === 'object') {
+
+                // product_cat.forEach(async category => {
+                //     const categId = await db_get(`SELECT id FROM categories WHERE category_name ="${category}"`);
+                //     await db_run(`INSERT INTO product_groups (category_id, product_id) VALUES (${categId.id}, ${+itemId})`);
+                // })
+
+                // product_cat.forEach(async category => {
+                for (let i = 0; i < product_cat.length; i++) {
+                    const categId = await db_get(`SELECT id FROM categories WHERE category_name ="${productcat[i]}"`);
+                    await db_run(`INSERT INTO product_groups (category_id, product_id) VALUES (${categId.id}, ${+itemId})`);
+                }
+                // })
+
+                // next()
             }
+
+            const categoryID = await db_get(`SELECT id FROM categories WHERE category_name = "${product_cat}"`);
+
+            console.log('product_cat', product_cat)
+            console.log('categoryID', categoryID)
+            await db_run(`INSERT INTO product_groups (category_id, product_id) VALUES (${categoryID.id}, ${+itemId})`);
+
+
         }
-        next();
     }
-    catch (err) { console.error(err) }
+    catch (err) { console.error(err); console.log(err) }
+    next();
 }
 
 const deleteProduct = async (req, res, next) => {
