683e038bfef2760eb7c4869bd6c2362a67b2a7da
Kozma János <41972874+JKozma81@users.noreply.github.com>
2020-03-07 18:39:22 +0100

    core functionality working but needs some handlebars workaround

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -20,18 +20,10 @@ module.exports = {
 		return options.inverse(this);
 	},
 
-	json: function (context) {
-		return JSON.stringify(context);
-	},
-
 	times: function (n, block) {
 		var accum = '';
 		for (var i = 1; i < n + 1; ++i)
 			accum += block.fn(i);
 		return accum;
-	},
-
-	concat: function (a, b) {
-		return a + b;
 	}
 };
--- models/category_middlewares.js
+++ models/category_middlewares.js
@@ -17,21 +17,30 @@ const getAlldata = async (req, res, next) => {
 
 
 		const mainsWithSubs = await db_getAll(`
-		SELECT maincategory.id AS main_cat_id,
-		maincategory.category_name AS main_cat_name,
-		GROUP_CONCAT(subcategory.category_name, ", ") AS subcategories
-		FROM categories AS maincategory
-		LEFT JOIN categories AS subcategory
-		ON maincategory.id = subcategory.parent_id
-		WHERE maincategory.parent_id IS NULL
-		GROUP BY maincategory.id
-		ORDER BY ${Object.keys(req.query).length === 0 ? 'main_cat_id' : orderingBy} ${Object.keys(req.query).length === 0
+			SELECT maincategory.id AS main_cat_id,
+			maincategory.category_name AS main_cat_name,
+			subcategory.id as sub_cat_id,
+			GROUP_CONCAT(subcategory.category_name, ", ") AS subcategories
+			FROM categories AS maincategory
+			LEFT JOIN categories AS subcategory
+			ON maincategory.id = subcategory.parent_id
+			WHERE maincategory.parent_id IS NULL
+			GROUP BY maincategory.id
+			ORDER BY ${Object.keys(req.query).length === 0 ? 'main_cat_id' : orderingBy} ${Object.keys(req.query).length === 0
 				? 'ASC'
 				: req.query.order}
-		LIMIT ${LIMIT}
-		OFFSET ${req.offset}`);
+			LIMIT ${LIMIT}
+			OFFSET ${req.offset}`);
 
-		const categoryHierarchy = await db_getAll('select maincategory.id as main_cat_id, maincategory.category_name as main_cat_name, subcategory.id as sub_cat_id, subcategory.category_name as subcategory from categories as maincategory left join categories as subcategory on maincategory.id = subcategory.parent_id where maincategory.parent_id is null');
+		const categoryHierarchy = await db_getAll(`
+			SELECT maincategory.id AS main_cat_id,
+			maincategory.category_name AS main_cat_name,
+			subcategory.id AS sub_cat_id,
+			subcategory.category_name AS subcategory
+			FROM categories AS maincategory
+			LEFT JOIN categories AS subcategory
+			ON maincategory.id = subcategory.parent_id
+			WHERE maincategory.parent_id IS NULL`);
 
 		const categoriyCount = await db_get('SELECT COUNT(id) AS items FROM categories where parent_id is null');
 
@@ -69,10 +78,52 @@ const modifyCategory = async (req, res, next) => {
 		const catId = req.params.id;
 		const { categ_name } = req.body;
 
-		if (typeof +catId === 'number') {
+		console.log(req.body)
+		/*
+		mainCatId: '1',
+		main_category_name: 'Számítástechnika',
+		subcategory_id: [ '12', '13' ],
+		subcategory_name: [ 'Alaplapok', 'Perifériák' ],
+		subcategorys_main_category: [ 'Számítástechnika', 'Számítástechnika' ],
+		change_to_main: [ '12 true', '13 true' ]
+		*/
+
+		if (typeof +catId === 'number' && categ_name) {
 			await db_run(`UPDATE categories SET category_name = "${categ_name}" WHERE id = ${+catId}`);
-			next();
+		} else if (typeof +catId === 'number') {
+			const {
+				main_catId,
+				main_category_name,
+				subcategory_id,
+				subcategory_name,
+				subcategorys_main_category,
+				change_to_main } = req.body;
+
+			await db_run(`UPDATE categories SET category_name = "${main_category_name}" WHERE id = ${+main_catId}`);
+
+			if (typeof subcategory_id === 'object' && typeof subcategory_name === 'object' && typeof subcategorys_main_category === 'object') {
+				for (const [idx, subcategoryId] of subcategory_id.entries()) {
+					await db_run(`UPDATE categories SET category_name = "${subcategory_name[idx]}" WHERE id = ${+subcategoryId}`);
+					const newParentId = await db_get(`SELECT id FROM categories WHERE category_name = "${subcategorys_main_category[idx]}"`)
+					await db_run(`UPDATE categories SET parent_id = ${newParentId.id} WHERE id = ${+subcategoryId}`);
+				}
+			} else {
+				await db_run(`UPDATE categories SET category_name = "${subcategory_name}" WHERE id = ${+subcategory_id}`);
+				const newParentId = await db_get(`SELECT id FROM categories WHERE category_name = "${subcategorys_main_category}"`)
+				await db_run(`UPDATE categories SET parent_id = ${newParentId.id} WHERE id = ${+subcategory_id}`);
+			}
+
+			if (change_to_main && typeof change_to_main === 'object') {
+				for (const categoryToChange of change_to_main) {
+					const tempArray = categoryToChange.split(' ');
+					await db_run(`UPDATE categories SET parent_id = NULL WHERE id = ${+tempArray[0]}`);
+				}
+			} else if (change_to_main && typeof change_to_main === 'string') {
+				const tempArray = categoryToChange.split(' ');
+				await db_run(`UPDATE categories SET parent_id = NULL WHERE id = ${+tempArray[0]}`)
+			}
 		}
+		next();
 	} catch (err) {
 		console.error(err);
 	}
